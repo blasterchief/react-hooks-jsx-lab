700be7036dd7e993c7135cdac16464d1648f855a
Conrad Etherington <conrad.etherington@gmail.com>
2022-11-03 06:25:49 -0600

    commit tree 77a1802bf3c7e0065cfcda7121b265db2638f248

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+      <h2>About Me</h2>
+      <p>I woke up early today after going to bed early.</p>
+      <img src={image} alt="I made this"/>
+        </div>;
 }
 
 export default About;
